d54ae1f9043dcdbfec70bcf80069cdc883edd49a
Shashi Singh <singh.shashishekhar@gmail.com>
2023-09-27 00:24:26 +0530

    feat: first checkin of the proactive website

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import proactiveLogo from "/proactive.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Proactive - React Components for Desktop</h1>
+      <div className="card">
+        <img src={proactiveLogo} className="logo" alt="Vite logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
