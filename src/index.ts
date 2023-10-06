a39a75e5b7cb11fce2e57bf5e7cebad33e122bb6
Shashi Singh <singh.shashishekhar@gmail.com>
2023-10-06 23:43:59 +0530

    fix: Fixed missing export of WizardStep

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./components/Icon/Icon";
 export * from "./components/MultiSelectGrid/MultiSelectGrid";
 export * from "./components/Surface/Surface";
 export * from "./components/Wizard/Wizard";
+export * from "./components/Wizard/WizardStep";
