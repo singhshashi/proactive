64296224b07a17167ec8480a2917bf4293c5e1bf
Shashi Singh <singh.shashishekhar@gmail.com>
2023-10-31 12:24:22 +0530

    feat: Added a SideNAv component

---
+++ src/components/SideNav/SideNav.stories.tsx
@@ -0,0 +1,57 @@
+import type { Story, Meta, StoryObj } from "@storybook/react";
+import { SideNav, SideNavProps } from "./SideNav";
+
+const meta = {
+  title: "Example/SideNav",
+  component: SideNav,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {},
+  tags: ["autodocs"],
+} satisfies Meta<typeof SideNav>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const navItems = [
+  {
+    label: "Shortcuts",
+    icon: "Bolt",
+    isSelected: true,
+    key: "Shortcuts",
+  },
+  {
+    label: "Screen To Record",
+    icon: "Tv",
+    isSelected: false,
+    key: "screenToRecord",
+  },
+  {
+    label: "Website Blocking",
+    icon: "NoSymbol",
+    isSelected: false,
+    key: "websiteBlocking",
+  },
+  {
+    label: "Focus Session",
+    icon: "VideoCamera",
+    isSelected: false,
+    key: "focusSession",
+  },
+];
+export const Default: Story = {
+  args: {
+    items: navItems,
+    onSelectionChange: (selectedItem: any) => {
+      navItems.forEach((item) => {
+        item.isSelected = false;
+        if (item.key === selectedItem.key) {
+          item.isSelected = true;
+          console.log("selected item: ", item);
+        }
+      });
+    },
+  },
+};
---
+++ src/components/SideNav/SideNav.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { Icon } from "../..";
+
+export type NavItem = {
+  key: string;
+  label: string;
+  icon: string;
+  isSelected: boolean;
+};
+
+export interface SideNavProps {
+  items: Array<NavItem>;
+  onSelectionChange: (selectedItem: NavItem) => void;
+}
+
+export const SideNav: React.FC<SideNavProps> = ({
+  items,
+  onSelectionChange,
+}) => {
+  return (
+    <div className="flex flex-col h-full ">
+      {items.map((item: NavItem, index: number) => {
+        const classes = `w-full px-2 py-2 flex items-center justify-start gap-3 ${
+          item.isSelected ? "bg-blue-300 text-white rounded" : ""
+        }`;
+
+        const iconColor = item.isSelected ? "#ffffff" : "#60a5fa";
+        return (
+          <div
+            className={classes}
+            key={`item-${index}`}
+            onClick={() => onSelectionChange(item)}
+          >
+            <Icon name={item.icon} dimension="24" color={iconColor} />
+            <div className="text-sm">{item.label}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
